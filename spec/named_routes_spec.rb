860d68dc7ef0538e94ef5cf000e3d6bc5e1d2434
Cristian Hampus <contact@cristianhampus.se>
2011-11-26 01:51:53 +0100

    Refactoring the spec

--- spec/named_routes_spec.rb
+++ spec/named_routes_spec.rb
@@ -2,64 +2,50 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Sinatra::NamedRoutes do
   def helper_route(&block)
-    result = nil
-    @helper_app.get('/helper_route') do
-      result = instance_eval(&block)
-      'ok'
-    end
-    get '/helper_route'
-    last_response.should be_ok
-    body.should be == 'ok'
-    result
-  end
-
-  before do
-    app = nil
-
     mock_app do
       register Sinatra::NamedRoutes
 
-      map(:hello, '/hello')
-      map(:path_multi_named, '/hello/:name.:format')
-      map(:path_multi_splat, '/hello/*.*')
-      map(:path_multi_regexp, %r{/hello/([\w]+).([\w]+)})
+      map(:path_named_params, '/hello/:name.:format')
+      map(:path_splats, '/hello/*.*')
+      map(:path_regexp, %r{/hello/([\w]+).([\w]+)})
       map(:path_named_captures, %r{/hello/(?<person>[^/?#]+)})
+      map(:path_optional_named_captures, %r{/page(?<format>.[^/?#]+)?})
 
-      get('/') { 'hello' }
-      get(:hello) { 'hello world' }
-      get('/hello/:name') { |name| "hello #{name}" }
-
-      app = self
+      get '/' do
+        instance_eval(&block)
+      end
     end
 
-    @helper_app = app
-  end
-
-  describe :helper_route do
-    it 'runs the block' do
-      ran = false
-      helper_route { ran = true }
-      ran.should be_true
-    end
-
-    it 'returns the block result' do
-      helper_route { 42 }.should be == 42
-    end
-
+    get('/')
   end
 
   describe :routing do
-    it 'does still allow normal routing' do
-      get('/').should be_ok
-      body.should be == 'hello'
+    it 'does allow routing with symbols as paths' do
+      mock_app do
+        register Sinatra::NamedRoutes
 
-      get('/hello/cristian').should be_ok
-      body.should be == 'hello cristian'
+        map :test, '/test'
+
+        get :test do
+          'symbols work'
+        end
+      end
+
+      get('/test').should be_ok
+      body.should be == 'symbols work'
     end
 
-    it 'does still allow routing with symbols as paths' do 
-      get('/hello').should be_ok
-      body.should be == 'hello world'
+    it 'does not break normal routing' do
+      mock_app do
+        register Sinatra::NamedRoutes
+
+        get '/normal' do
+          'still works'
+        end
+      end
+
+      get('/normal').should be_ok
+      body.should be == 'still works'
     end
 
   end
@@ -69,38 +55,46 @@ describe Sinatra::NamedRoutes do
     it 'does not break normal behavior' do
       helper_route do
         url '/route_one', false
-      end.should be == '/route_one'
+      end
+
+      body.should be == '/route_one'
     end
 
     it 'ignores params if path is not a symbol' do
       helper_route do
         url '/route_one', false, :name => 'cristian'
-      end.should be == '/route_one'
+      end
+
+      body.should be == '/route_one'
     end
 
     describe :named do
-      it 'returns the correct path if passed a hash with symbols as keys' do 
+      it 'supports named parameters' do 
         helper_route do
-          url :path_multi_named, false, :name => 'cristian', :format => 'json'  
-        end.should be == '/hello/cristian.json'
-      end
+          url :path_named_params, false, :name => 'cristian', :format => 'json'  
+        end
 
-      it 'returns the correct path if passed a hash with strings as keys' do
+        body.should be == '/hello/cristian.json'
+
         helper_route do
-          url :path_multi_named, false, 'name' => 'cristian', 'format' => 'json'  
-        end.should be == '/hello/cristian.json'
+          url :path_named_params, false, 'name' => 'cristian', 'format' => 'json'  
+        end
+
+        body.should be == '/hello/cristian.json'
       end
 
       it 'ignores keys that are left' do
         helper_route do
-          url :path_multi_named, false, :name => 'cristian', :format => 'json', :color => 'blue'  
-        end.should be == '/hello/cristian.json'
+          url :path_named_params, false, :name => 'cristian', :format => 'json', :color => 'blue'  
+        end
+
+        body.should be == '/hello/cristian.json'
       end
 
       it 'throws an exception if required keys do not exist' do
         expect do
           helper_route do
-            url :path_multi_named, false, :name => 'cristian', :color => 'blue'
+            url :path_named_params, false, :name => 'cristian', :color => 'blue'
           end
         end.to raise_exception ArgumentError
       end
@@ -110,14 +104,16 @@ describe Sinatra::NamedRoutes do
     describe :splat do
       it 'returns the correct url for path with splats' do
         helper_route do
-          url :path_multi_splat, false, ['cristian', 'json'] 
-        end.should be == '/hello/cristian.json'
+          url :path_splats, false, ['cristian', 'json'] 
+        end
+
+        body.should be == '/hello/cristian.json'
       end
 
       it 'throws exception if params does not match number of splats' do
         expect do
           helper_route do
-            url :path_multi_splat, false, ['cristian']
+            url :path_splats, false, ['cristian']
           end
         end.to raise_exception ArgumentError
       end
@@ -127,14 +123,16 @@ describe Sinatra::NamedRoutes do
     describe :regular_expression do
       it 'returns the correct url for path with regular expressions' do
         helper_route do
-          url :path_multi_regexp, false, ['cristian', 'json'] 
-        end.should be == '/hello/cristian.json'
+          url :path_regexp, false, ['cristian', 'json'] 
+        end
+
+        body.should be == '/hello/cristian.json'
       end
 
       it 'throws exception if params does not match number of captures' do
         expect do
           helper_route do
-            url :path_multi_regexp, false, ['cristian']
+            url :path_regexp, false, ['cristian']
           end
         end.to raise_exception ArgumentError
       end
@@ -146,7 +144,31 @@ describe Sinatra::NamedRoutes do
 
         helper_route do
           url :path_named_captures, false, :person => 'cristian'
-        end.should be == '/hello/cristian'
+        end
+
+        body.should be == '/hello/cristian'
+      end
+
+      it 'returns the correct url for path with optional named captures' do
+        next if RUBY_VERSION < '1.9'
+
+        helper_route do
+          url :path_optional_named_captures, false, :format => 'html'
+        end
+
+        body.should be == '/page.html'
+
+        helper_route do
+          url :path_optional_named_captures, false, :format => 'xml'
+        end
+
+        body.should be == '/page.xml'
+
+        helper_route do
+          url :path_optional_named_captures, false
+        end
+
+        body.should be == '/page'
       end
     end
 
